07ca2d5388040fbce10b4ace6ffe03fd4669f3e2
shailesh-sangekar <shailesh.sangekar@eternussolutions.com>
2016-11-22 19:21:27 +0530

    Bug fixes for Bug #10763

--- src/client/app/shared/components/spinner/spinner.component.ts
+++ src/client/app/shared/components/spinner/spinner.component.ts
@@ -1,22 +1,23 @@
-import {Component, OnDestroy, OnInit,EventEmitter} from '@angular/core';
+import {Component, OnDestroy, OnInit, EventEmitter} from '@angular/core';
 
 import { SpinnerService } from './spinner.service';
 
 @Component({
   selector: 'story-spinner',
   template: `
-    <div class="spinner" *ngIf="visible"> <i class="fa fa-spinner fa-spin" style="font-size:50px"></i></div>`,
-  styles: ['.spinner {position: absolute;left: 46%;top: 50%'],
+    <div class="overlay" *ngIf="visible" ><div class="spinner" > <i class="fa fa-spinner fa-spin" style="font-size:75px"></i></div></div>`,
+  //styles: ['.spinner {position: absolute;left: 46%;top: 50%;} #overlay {position: absolute;left: 0;top: 0;bottom: 0;right: 0;background: #000;opacity: 0.8;filter: alpha(opacity=80);}'],
+  styles: ['.spinner { width: 50px;height: 75px;position: absolute;top: 50%;left: 50%;margin: -28px 0 0 -25px;} .overlay {position: fixed;left: 0;top: 0;bottom: 0;right: 0;background: rgba(85, 85, 85, 0.71);opacity: 0.9;filter: alpha(opacity=80);}'],
 })
 
 export class SpinnerComponent implements OnDestroy, OnInit {
-  visible:boolean = false;
+  visible: boolean = false;
   subscription: EventEmitter<boolean> = new EventEmitter<boolean>();;
   constructor(private _spinnerService: SpinnerService) { }
 
   ngOnInit() {
-        this.subscription = this._spinnerService.getSpinnerEmitter()
-            .subscribe((value: boolean) => { this.visible = value; });
+    this.subscription = this._spinnerService.getSpinnerEmitter()
+      .subscribe((value: boolean) => { this.visible = value; });
   }
 
   ngOnDestroy() {
