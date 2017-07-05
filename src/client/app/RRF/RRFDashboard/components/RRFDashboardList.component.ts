8d4347b25dc8218b7adbeba61611e20ee526b5b4
chetanbadgujar9 <chetanbadgujar9@gmail.com>
2017-07-05 18:08:08 +0530

    resolved incorrect time bug on rrf dashboard

--- src/client/app/RRF/RRFDashboard/components/RRFDashboardList.component.ts
+++ src/client/app/RRF/RRFDashboard/components/RRFDashboardList.component.ts
@@ -646,5 +646,16 @@ export class RRFDashboardListComponent implements OnActivate {
         }
         this.onViewChanged(this.currentView)
     }
+     //Format date in "yyyy-mm-dd" format
+    formatDate(date: any) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [day, month, year].join('-');
+    }
 }
 
