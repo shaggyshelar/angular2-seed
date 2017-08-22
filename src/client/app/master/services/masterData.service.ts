e34ad68b65a5763f48eec21d82aca8eac7f5199e
sayali-eternus <sayali.tidke@eternussolutions>
2017-08-22 16:13:03 +0530

    service added for skype interview page

--- src/client/app/master/services/masterData.service.ts
+++ src/client/app/master/services/masterData.service.ts
@@ -5,17 +5,75 @@ import { AuthHttp } from '../../shared/services/authHttp.service';
 import { Config } from '../../shared/config/config';
 import { SpinnerService } from '../../shared/components/spinner/spinner';
 import { GrdOptions } from '../../shared/model/index';
+import { SkypeMaster,InterviewMode } from '../model/masterModel';
 
 @Injectable()
 export class MyMasterDataService {
 
     constructor(private http: Http, private authHttp: AuthHttp, private _spinnerService: SpinnerService) { }
 
-
+    /** GET SKYPE DETAILS FOR THE CANDIDATE PROFILES FROM BACKEND*/
     getSkypeData() {
         let url = Config.GetURL('/api/Masters/GetSkypeIDsMaster');
         this._spinnerService.show();
         return this.authHttp.get(url)
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+    addSkypeData(SkypeCredential : SkypeMaster) {
+        let url = Config.GetURL('/api/Masters/AddSkypeCredentials');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { SkypeCredential })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+    editSkypeData(SkypeCredential : SkypeMaster) {
+        let url = Config.GetURL('/api/Masters/UpdateSkypeCredentials');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { SkypeCredential })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+     deleteSkypeData(SkypeCredential : SkypeMaster) {
+        let url = Config.GetURL('/api/Masters/DeleteSkypeCredentials');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { SkypeCredential })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+/** GET Interview Mode DETAILS FOR THE CANDIDATE PROFILES FROM BACKEND*/
+    getInterviewModeData() {
+        let url = Config.GetURL('/api/Masters/GetInterviewMode');
+        this._spinnerService.show();
+        return this.authHttp.get(url)
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+    addInterviewModeData(Mode : InterviewMode) {
+        let url = Config.GetURL('/api/Masters/AddInterviewMode');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { Mode })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+    editInterviewModeData(Mode : InterviewMode) {
+        let url = Config.GetURL('/api/Masters/UpdateInterviewMode');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { Mode })
+            .map(this.extractData)
+            .catch(this.handleError)
+            .finally(() => this._spinnerService.hide());
+    }
+     deleteInterviewModeData(Mode : InterviewMode) {
+        let url = Config.GetURL('/api/Masters/DeleteInterviewMode');
+        this._spinnerService.show();
+        return this.authHttp.post(url, { Mode })
             .map(this.extractData)
             .catch(this.handleError)
             .finally(() => this._spinnerService.hide());
@@ -38,16 +96,7 @@ export class MyMasterDataService {
             .catch(this.handleError)
             .finally(() => this._spinnerService.hide());
     }
-    // addCandidateProfile(profile: CandidateProfile) {
-    //     let url = Config.GetURL('/api/ProfileBank/addCandidateProfile');
-    //     this._spinnerService.show();
-    //     return this.authHttp.post(url, { profile })
-    //         .map(this.extractData)
-    //         .catch(this.handleError)
-    //         .finally(() => this._spinnerService.hide());
-    // }
-
-
+  
     private extractData(res: Response) {
         if (res.status < 200 || res.status >= 300) {
             throw new Error('Bad response status: ' + res.status);
